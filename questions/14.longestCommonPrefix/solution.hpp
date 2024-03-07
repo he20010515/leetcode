50939fb540248371f10a8dff5fc10475a51061d5
heyuwei <he20010515@163.com>
2024-03-07 12:00:28 +0800

    problem 14

--- questions/14.longestCommonPrefix/solution.hpp
+++ questions/14.longestCommonPrefix/solution.hpp
@@ -2,15 +2,33 @@
  * @Author: heyuwei he2001015@163.com
  * @Date: 2024-02-27 21:26:47
  * @LastEditors: heyuwei he2001015@163.com
- * @LastEditTime: 2024-03-06 09:48:27
+ * @LastEditTime: 2024-03-07 11:57:48
  * @FilePath: /leetcode/questions/twosum/solution.hpp
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置
  * 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 #include "basic.hpp"
+#include <string>
+#include <vector>
 using namespace std;
 class Solution
 {
 public:
-    int sol(int a) { return 0; }
+    string sol(vector<string> &strs)
+    {
+        auto p = 0;
+        auto temp_char = '\0';
+        while (true)
+        {
+            temp_char = strs[0].c_str()[p];
+            for (auto &&str : strs)
+            {
+                if (temp_char != str[p] or temp_char == '\0')
+                {
+                    return str.substr(0, p);
+                }
+            }
+            p++;
+        }
+    }
 };
--- questions/14.longestCommonPrefix/units.hpp
+++ questions/14.longestCommonPrefix/units.hpp
@@ -2,21 +2,30 @@
  * @Author: heyuwei he2001015@163.com
  * @Date: 2024-02-26 14:02:54
  * @LastEditors: heyuwei he2001015@163.com
- * @LastEditTime: 2024-03-06 17:31:48
+ * @LastEditTime: 2024-03-07 11:58:53
  * @FilePath: /leetcode/quentions/1.twosum/tests.cpp
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置
  * 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 #include <iostream>
+#include <string>
 #include <tuple>
 #include <vector>
 
 using namespace std;
-using InputType = tuple<int>;
-using OutputType = tuple<double>;
+using InputType = tuple<vector<string>>;
+using OutputType = tuple<string>;
 
-vector<InputType> TEST_UNIT_INPUT = {};
-vector<OutputType> TEST_UNIT_OUTPUT = {};
+vector<InputType> TEST_UNIT_INPUT = {
+    {{"flower", "flow", "flight"}},
+    {{"dog", "racecar", "car"}},
+    {{"", "racecar", ""}},
+};
+vector<OutputType> TEST_UNIT_OUTPUT = {
+    {"fl"},
+    {""},
+    {""},
+};
 
 inline bool compare(OutputType ground_truth, OutputType res)
 {
