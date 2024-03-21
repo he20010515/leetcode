9a39470c9d085120f93e6d5a725d84260f088c78
heyuwei <he20010515@163.com>
2024-03-21 16:21:59 +0800

    problem 55

--- questions/55.canjump/solution.hpp
+++ questions/55.canjump/solution.hpp
@@ -2,7 +2,7 @@
  * @Author: heyuwei he2001015@163.com
  * @Date: 2024-02-27 21:26:47
  * @LastEditors: heyuwei he20010515@163.com
- * @LastEditTime: 2024-03-21 15:31:50
+ * @LastEditTime: 2024-03-21 16:21:48
  * @FilePath: /leetcode/questions/twosum/solution.hpp
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置
  * 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
@@ -13,7 +13,19 @@ using namespace std;
 class Solution
 {
 public:
-    bool sol(vector<int> &nums){
-
+    bool sol(vector<int> &nums)
+    {
+        int n = nums.size();
+        int rightmost = 0;
+        for (int i = 0; i < n; ++i)
+        {
+            if (i <= rightmost)
+            {
+                rightmost = max(rightmost, i + nums[i]);
+                if (rightmost >= n - 1)
+                    return true;
+            }
+        }
+        return false;
     };
 };
--- questions/55.canjump/units.hpp
+++ questions/55.canjump/units.hpp
@@ -2,7 +2,7 @@
  * @Author: heyuwei he2001015@163.com
  * @Date: 2024-02-26 14:02:54
  * @LastEditors: heyuwei he20010515@163.com
- * @LastEditTime: 2024-03-21 15:23:46
+ * @LastEditTime: 2024-03-21 16:19:39
  * @FilePath: /leetcode/quentions/1.twosum/tests.cpp
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置
  * 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
