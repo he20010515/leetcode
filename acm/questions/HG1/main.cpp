75ff593e9f32d843ebc1d5ab52a927342cece7d9
heyuwei <he20010515@163.com>
2024-04-03 23:07:16 +0800

    nowcoder

---
+++ acm/questions/HG1/main.cpp
@@ -0,0 +1,16 @@
+/*
+ * @Author: heyuwei he20010515@163.com
+ * @Date: 2024-04-01 20:17:43
+ * @LastEditors: heyuwei he20010515@163.com
+ * @LastEditTime: 2024-04-01 20:49:23
+ * @FilePath: /leetcode/acm/questions/HG1/main.cpp
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    std::string temp;
+    std::cin >> temp;
+    std::cout << temp;
+}
